24d88788da6167355eaabc6ba9c3accfa42c04aa
Danwlee <lebaodang303@gmail.com>
2025-06-23 03:15:15 +0700

    add remain ex

---
+++ Lab-1/src/RemainEx.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class RemainEx {
+    static void Ex5_1 () {
+        System.out.println("hello world");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Ex5_1();
+    }
+}
