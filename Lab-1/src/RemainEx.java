47fd73c75a15328f8307616138d3f1989367d3d9
Danwlee <lebaodang303@gmail.com>
2025-06-23 17:52:22 +0700

    update Ex 5.1

--- Lab-1/src/RemainEx.java
+++ Lab-1/src/RemainEx.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
 public class RemainEx {
     static void Ex5_1 () {
-        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input an integer: ");
+        int num = sc.nextInt();
+        if (num >=10 && num <= 100) {
+            System.out.println(num + " is in the range [10,100].");
+        } else {
+            System.out.println(num + " is not in the range [10,100].");
+        }
     }
     static void Prac5_4 () {
         Scanner sc = new Scanner(System.in);
